4a3f10de78d838f0a089164ae5ebd6fac5aa8754
Mohammad Usman Nasir <usmannasir98@gmail.com>
2024-07-02 20:44:39 +0500

    Edited

---
+++ Weather_App/script.js
@@ -0,0 +1,45 @@
+const apikey = "";
+const apiURL = "";
+
+const searchBox = document.querySelector (".search input");
+const searchBtn = document.querySelector (".search button");
+const WeatherIcon = document.querySelector (".Weather-Icon");
+
+async function checkWeather (city) {
+    const response = await fetch (apiURL + city + `&appid=${apikey}`);
+
+    If(response.status == 404){
+        document.querySelector (".error").style.display = "block";
+        document.querySelector (".weather").style.display = "none";
+    }else{
+        var data = await response.json ();
+        
+        document.querySelector (".city").innerHTML = data.name;
+        document.querySelector (".temp").innerHTML = Math.round (data.main.temp)  + "°C";
+        document.querySelector (".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector (".wind").innerHTML = data.wind.speed + " km/h";
+    }
+        if (data.weather [0].main == "Clouds") {
+            Weather-Icon.src; "../../Images/weather-app-img/clouds.png";
+        }
+    
+    else if (data.weather [0].main == "Clear") {
+    Weather-Icon.src; "../../Images/weather-app-img/clear.png";
+    }
+    else if (data.weather [0].main == "Rain") {
+    Weather-Icon.src; "../../Images/weather-app-img/rain.png";
+    }
+    else if (data.weather [0].main == "Drizzle") {
+    Weather-Icon.src; "../../Images/weather-app-img/drizzle.png";
+    }
+    else if (data.weather [0].main == "Mist") {
+    Weather-Icon.src; "../../Images/weather-app-img/mist.png";
+    }
+
+    document.querySelector (".weather").style.display = "block";
+    document.querySelector (".error").style.display = "none";
+}
+
+searchBtn.addEventListener ("click", () => {
+checkWeather (searchBox.value);
+})
